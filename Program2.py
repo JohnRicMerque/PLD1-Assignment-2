3630a1824cb160543526d28131cdfaa273c9717a
unknown <you@example.com>
2021-10-24 09:01:14 +0800

    Program complete: computation is functional

--- Program2.py
+++ Program2.py
@@ -2,8 +2,15 @@
 
 # Storing input in variables
 numberOfApples = int(input("Enter the number of apples you want to buy: "))
-numberOfOranges = int(input("Enter the number of apples you want to buy: "))
+numberOfOranges = int(input("Enter the number of oranges you want to buy: "))
 
 # Price scheme
 aplPrice = 20
 orgPrice = 25
+
+# Computation
+aplTotalCost = numberOfApples * aplPrice
+orgTotalCost = numberOfOranges * orgPrice
+total = aplTotalCost + orgTotalCost
+
+print(f"The total amount is {total}.")
