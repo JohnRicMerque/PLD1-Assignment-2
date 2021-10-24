5769aabf2745e9774fa448804c41d8c24b45d9aa
unknown <you@example.com>
2021-10-24 10:31:12 +0800

    assigned variables

---
+++ Program3.py
@@ -0,0 +1,8 @@
+""" 
+This program computes the maximum number of apples that can be bought 
+for a specific amount of money and displays change as well
+"""
+
+# asks user for input
+moneyAmount = float(input("How much money do you have?: "))
+aplPrice = float(input("Enter the price of an apple: "))
