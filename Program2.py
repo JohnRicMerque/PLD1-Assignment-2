71e6210146b02aecda845eac583c67d9c8a0a284
unknown <you@example.com>
2021-10-24 08:47:00 +0800

    storing data in variables

---
+++ Program2.py
@@ -0,0 +1,9 @@
+# This program asks how many apples and oranges user wants to buy and displays total price
+
+# Storing input in variables
+numberOfApples = int(input("Enter the number of apples you want to buy: "))
+numberOfOranges = int(input("Enter the number of apples you want to buy: "))
+
+# Price scheme
+aplPrice = 20
+orgPrice = 25
