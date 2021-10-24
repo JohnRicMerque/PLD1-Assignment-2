11ea09c827cd876baea8796fe1b89beda3f12d74
unknown <you@example.com>
2021-10-24 08:04:43 +0800

    user input stored in variables

---
+++ Program1.py
@@ -0,0 +1,5 @@
+# This program asks the user for name, age and address and displays it
+
+name = input("What's your name?: ")
+age = input("How old are you?: ")
+address = input("Where do you live?: ")
