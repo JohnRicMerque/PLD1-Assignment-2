a5ad9ff22ca3367f0b4f9d99ece6c11da6c2e089
unknown <you@example.com>
2021-10-24 08:06:16 +0800

    Program complete: successfully displays user input

--- Program1.py
+++ Program1.py
@@ -3,3 +3,5 @@
 name = input("What's your name?: ")
 age = input("How old are you?: ")
 address = input("Where do you live?: ")
+
+print(f"Hi, my name is {name}. I am {age} years old and I live in {address}.")
